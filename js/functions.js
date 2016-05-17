8d89a0e889fac195bde2e36e757902ce45c09a64
Don Westendorp <donwebdeveloper@gmail.com>
2016-05-17 07:27:18 -0500

    Finalized project
    Woke up and double checked everything, we're good to go on current specs
    at least

--- js/functions.js
+++ js/functions.js
@@ -9,11 +9,18 @@ function load_index_functions() {
 	parallax_3d('cash-back');
 	parallax_3d('prizes');
 	
+	// Initialize slide ins
+	slide_in_controller();
+	
+	// Initialize countdown timers
+	countdown_timers();	
 	// Do parallax calculations on scroll here
+	// Do slide in calculations here as well
 	$(window).scroll(function(){
 		parallax_3d('discounts');
 		parallax_3d('cash-back');
 		parallax_3d('prizes');
+		slide_in();
 	});
 }
 
@@ -29,8 +36,7 @@ function parallax_3d(id) {
 	
 	// Check if the item is visible, if so calculate parallax 
 	if((scroll_top + viewport_height) > scroll_height && scroll_top < (item_height + scroll_height)) {
-			
-		
+				
 		// Get the ratio between the scroll position and viewport height
 		var ratio = parseFloat((scroll_height - scroll_top) / viewport_height);
 		
@@ -45,10 +51,97 @@ function parallax_3d(id) {
 	}
 }
 
-function slide_out_controller() {
+function slide_in_controller() {
+	
+	// Get all slideins on the page
+	var slide_ins = $('.slidein-box');
+	
+	$.each(slide_ins,function(key, slide_in) {
+				
+		// Set their opactity to 0
+		$(slide_in).css('opacity','0.0');
+		
+	});
+}
+
+
+function slide_in() {
+	
+	// Get all slideins on the page
+	var slide_ins = $('.slidein-box');
+	
+	var scroll_bottom = parseInt($(window).scrollTop()) +  parseInt($( window ).height());
+		
+	var duration = 1000;
+	
+	$.each(slide_ins,function(key, slide_in) {		
+		
+		// See if object can be slidden in
+		// Object must be hidden and must be at least 100 pixels on screen in order to slide in
+		if($(slide_in).css('opacity')==0 && $(slide_in).offset().top < (scroll_bottom - 100) && $(slide_in).offset().top > ($(window).scrollTop() - 100)) {
+			
+			var left = $(slide_in).position('left').left;			
+				
+			$(slide_in).css({left:(left + 1000)+'px', opacity: 1.0}).animate({left: '0px'}, {duration: duration, easing: 'easeOutBounce'}).hover(
+		
+			// Hover function
+			function() {
+			
+				$(this).switchClass(' slidein-box-normal','slidein-box-hover', 300);
+			
+			// Unhover function	
+			}, function() {
+			
+				$(this).switchClass('slidein-box-hover','slidein-box-normal', 300);				
+				
+		});		
+		
+			duration += 400;
+		}
+		
+	});	
+}
+
+function countdown_timers() {
+	
+	// Start each timer thats on the page
+	var timers = $('[class*=-time-left]');
+	
+	$.each(timers,function(key, timer) {
+		setInterval(function() { countdown_time(timer); },1000);
+		
+	});
 	
 }
 
-function slide_out(id) {
+// Decrement timer by one second
+function countdown_time(timer) {
+	
+		var time = $(timer).html();
+		
+		time = time.split(' : ');
+		
+		var hours = time[0].split(' ');
+		hours = parseInt(hours[0]);
+		
+		var mins = time[1].split(' ');
+		mins = parseInt(mins[0]);
+		
+		var secs = time[2].split(' ');
+		secs = parseInt(secs[0]);
+		
+		secs--;
+		
+		if(secs < 0) { 
+			secs = 59;
+			mins--;
+		}
+		
+		if(mins < 0 ) {
+			mins = 59;
+			hours--;
+		}
+		
+		$(timer).html(hours+' hr : '+mins+' min : '+secs+' sec');
 	
 }
